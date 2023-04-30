3cc4af64d18ee6345a67a41b445a0c21597f3931
CrazyCatV5 <zahark13@gmail.com>
2023-05-01 02:42:07 +1000

    Выбор по id готов. Ненавижу докер

--- main/views.py
+++ main/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from main.models import Product, ProductColor
 from main.serializers import ProductSerializer
-
-
+from rest_framework import viewsets
 # Create your views here.
 
 
@@ -20,9 +20,13 @@ def about(request):
 
 class ProductAPI(APIView):
     def get(self, request):
-        products = Product.objects.all().values()
-        color = Product.objects.all()
-        return Response({'products': ProductSerializer(color, many=True).data})
+        products = Product.objects.all()
+        return Response({'products': ProductSerializer(products, many=True).data})
 
     def post(self, request):
         return Response()
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
