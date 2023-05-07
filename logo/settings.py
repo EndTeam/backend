eb41200f5b8ddd4da5a4ca252d92d33032854944
CrazyCatV5 <zahark13@gmail.com>
2023-05-08 03:05:46 +1000

    hotfix

--- logo/settings.py
+++ logo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SK
-DATABASE_PATH = 'localhost'
+DATABASE_PATH = 'db'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
